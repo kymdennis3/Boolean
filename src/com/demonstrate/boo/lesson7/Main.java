4ca8e24b6ecd235aa990ada13fc24df6309167bb
kymdennis3 <kymdennis3@gmail.com>
2022-11-23 19:04:42 +0300

    Second Commit

---
+++ src/com/demonstrate/boo/lesson7/Main.java
@@ -0,0 +1,22 @@
+
+package com.demonstrate.boo.lesson7;
+
+import org.junit.jupiter.api.Test;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 4;
+        int j = 3;
+        boolean monday = false;
+
+        if (monday == true) {
+            j = 99;
+        } else if (j > i) {
+
+        } else if (j == i) {
+
+        }
+
+
+    }
